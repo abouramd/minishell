c278d7c6cf37072a74c8c9311ec7b77e2e69c34a
abdelhay bouramdane <109427562+abouramd@users.noreply.github.com>
2023-03-10 09:26:45 +0000

    @abouramd

--- parsing/open_files.c
+++ parsing/open_files.c
@@ -3,51 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   open_files.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
+/*   By: abouramd <abouramd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 00:38:07 by zasabri           #+#    #+#             */
-/*   Updated: 2023/03/08 19:06:40 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/03/10 09:20:23 by abouramd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include.h"
 
-void	for_out_redirection(t_data *f, t_vals *first, t_cmd_list *s,
+char	**m_file(t_data *f, int file_fd, char *file_name, char *val)
+{
+	char	**name;
+
+	f->ambiguous = 0;
+	if (file_fd != 1 && file_fd != 0)
+		close(file_fd);
+	if (file_name)
+		free(file_name);
+	name = ft_expand_str(f, val);
+	if (!name || !name[0] || name[1])
+		f->ambiguous = 1;
+	return (name);
+}
+
+void	for_out_redirection(t_data *f, t_vals *first, t_cmd_list *save,
 		t_list **lexer)
 {
 	char	**name;
 
 	*lexer = (*lexer)->next;
 	first = (t_vals *)(*lexer)->content;
-	if (first->token == V_STR)
+	if (first->token == V_STR && save->outfile > 0 && save->infile >= 0)
 	{
-		if (s->outfile > 0 && s->infile >= 0)
+		name = m_file(f, save->outfile, save->outfile_name, first->val);
+		if (!f->ambiguous)
 		{
-			if (s->outfile != 1)
-				close(s->outfile);
-			f->ambiguous = 0;
-			if (s->outfile_name)
-				free(s->outfile_name);
-			name = ft_expand_str(f, first->val);
-			if (!name || !name[0] || name[1])
-				f->ambiguous = 1;
-			if (!f->ambiguous)
+			save->outfile = open(name[0], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+			if (save->outfile < 0)
 			{
-				s->outfile = open(name[0], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-				if (s->outfile < 0)
-				{
-					s->outfile_errno = errno;
-					s->outfile_name = ft_strdup(name[0]);
-				}
-				ft_free(name);
+				save->outfile_errno = errno;
+				save->outfile_name = ft_strdup(name[0]);
 			}
-			else
-			{
-				ft_free(name);
-				s->outfile = -1;
-				s->outfile_errno = 300;
-				s->outfile_name = ft_strdup(first->val);
-			}
+			ft_free(name);
+		}
+		else
+		{
+			ft_free(name);
+			save->outfile = -1;
+			save->outfile_errno = 300;
+			save->outfile_name = ft_strdup(first->val);
 		}
 	}
 }
@@ -59,35 +64,25 @@ void	for_input_redirection(t_data *f, t_vals *first, t_cmd_list *save,
 
 	*lexer = (*lexer)->next;
 	first = (t_vals *)(*lexer)->content;
-	if (first->token == V_STR)
+	if (first->token == V_STR && save->outfile > 0 && save->infile >= 0)
 	{
-		if (save->outfile > 0 && save->infile >= 0)
+		name = m_file(f, save->infile, save->infile_name, first->val);
+		if (!f->ambiguous)
 		{
-			if (save->infile != 0 && save->infile > 0)
-				close(save->infile);
-			f->ambiguous = 0;
-			if (save->infile_name)
-				free(save->infile_name);
-			name = ft_expand_str(f, first->val);
-			if (!name || !name[0] || name[1])
-				f->ambiguous = 1;
-			if (!f->ambiguous)
+			save->infile = open(name[0], O_RDONLY, 0644);
+			if (save->infile < 0)
 			{
-				save->infile = open(name[0], O_RDONLY, 0644);
-				if (save->infile < 0)
-				{
-					save->infile_errno = errno;
-					save->infile_name = ft_strdup(name[0]);
-				}
-				ft_free(name);
+				save->infile_errno = errno;
+				save->infile_name = ft_strdup(name[0]);
 			}
-			else
-			{
-				ft_free(name);
-				save->outfile = -1;
-				save->outfile_errno = 300;
-				save->outfile_name = ft_strdup(first->val);
-			}
+			ft_free(name);
+		}
+		else
+		{
+			ft_free(name);
+			save->outfile = -1;
+			save->outfile_errno = 300;
+			save->outfile_name = ft_strdup(first->val);
 		}
 	}
 }
@@ -98,35 +93,25 @@ void	for_append(t_data *f, t_vals *first, t_cmd_list *s, t_list **lexer)
 
 	*lexer = (*lexer)->next;
 	first = (t_vals *)(*lexer)->content;
-	if (first->token == V_STR)
+	if (first->token == V_STR && s->outfile > 0 && s->infile >= 0)
 	{
-		if (s->outfile > 0 && s->infile >= 0)
+		name = m_file(f, s->outfile, s->outfile_name, first->val);
+		if (!f->ambiguous)
 		{
-			if (s->outfile != 1 && s->outfile > 0)
-				close(s->outfile);
-			f->ambiguous = 0;
-			if (s->outfile_name)
-				free(s->outfile_name);
-			name = ft_expand_str(f, first->val);
-			if (!name || !name[0] || name[1])
-				f->ambiguous = 1;
-			if (!f->ambiguous)
+			s->outfile = open(name[0], O_CREAT | O_WRONLY | O_APPEND, 0644);
+			if (s->outfile < 0)
 			{
-				s->outfile = open(name[0], O_CREAT | O_WRONLY | O_APPEND, 0644);
-				if (s->outfile < 0)
-				{
-					s->outfile_errno = errno;
-					s->outfile_name = ft_strdup(name[0]);
-				}
-				ft_free(name);
+				s->outfile_errno = errno;
+				s->outfile_name = ft_strdup(name[0]);
 			}
-			else
-			{
-				ft_free(name);
-				s->outfile = -1;
-				s->outfile_errno = 300;
-				s->outfile_name = ft_strdup(first->val);
-			}
+			ft_free(name);
+		}
+		else
+		{
+			ft_free(name);
+			s->outfile = -1;
+			s->outfile_errno = 300;
+			s->outfile_name = ft_strdup(first->val);
 		}
 	}
 }
